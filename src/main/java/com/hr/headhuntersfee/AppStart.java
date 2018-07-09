f867690fd9391d63543eff217027f0da9657cf4a
Pasan Manudul Hewawaduge <pasan@crowderia.com>
2018-07-09 11:11:18 +0530

    configd appstart

--- src/main/java/com/hr/headhuntersfee/AppStart.java
+++ src/main/java/com/hr/headhuntersfee/AppStart.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HeadhuntersfeeApplication {
+public class AppStart {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HeadhuntersfeeApplication.class, args);
+		SpringApplication.run(AppStart.class, args);
 	}
 }
--- src/test/java/com/hr/headhuntersfee/AppStartTests.java
+++ src/test/java/com/hr/headhuntersfee/AppStartTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class HeadhuntersfeeApplicationTests {
+public class AppStartTests {
 
 	@Test
 	public void contextLoads() {
