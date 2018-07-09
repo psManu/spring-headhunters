bb826850d428b6c26800f09e686e42a557e902cc
Pasan Manudul Hewawaduge <pasan@crowderia.com>
2018-07-09 11:01:29 +0530

    init push

---
+++ src/main/java/com/hr/headhuntersfee/HeadhuntersfeeApplication.java
@@ -0,0 +1,12 @@
+package com.hr.headhuntersfee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadhuntersfeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadhuntersfeeApplication.class, args);
+	}
+}
